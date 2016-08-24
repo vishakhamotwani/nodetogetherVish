93e08392536b4b41bbe443b25d580e6b4005bbee
Vishakha Motwani <vishakha.motwani@gmail.com>
2016-08-23 21:07:38 -0400

    Server bana bhi aur chala bhi

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-require('express');
+var express = require('express');
+var app = express();
+
+var port = "8080";
+var host = "0.0.0.0";
+
+app.listen(port, host);
+
+console.log("What do you think happened?");
